d85f038695cebcaaf0dfd8fc269c4e4e6e1f0ab1
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-04-05 17:39:07 +0000

    Refactored the form with form_with

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -46,10 +46,8 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie  = Movie.find(params.fetch(:id))
-
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
